285b134af8f9ebbc572e526f6112edbcee0269a1
Jonathan <rochacontrerasjonathan40@gmail.com>
2026-04-28 12:19:25 -0400

    se cambio la llamada para definir home como NotasPage y no notas_header como antes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_duo/features/notas/widgets/notas_header.dart';
+import 'package:flutter_duo/features/notas/pages/notas_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: NotasHeader());
+    return const MaterialApp(home: NotasPage());
   }
 }
